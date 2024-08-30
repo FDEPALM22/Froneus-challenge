41363c1bd708efd701de855e8b6c342f5972060c
Fernando <depalmafernandoemilio@gmail.com>
2024-08-30 20:49:50 -0300

    primera implementacion

---
+++ challenge/src/test/java/com/depalma/froneus/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depalma.froneus.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
